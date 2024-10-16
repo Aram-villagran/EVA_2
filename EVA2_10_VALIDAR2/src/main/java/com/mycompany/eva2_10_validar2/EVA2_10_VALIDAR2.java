601262d045d3406d2825c7b1d51d2b18f8d612f7
Aram-villagran <L24550334@chihuahua2.tecnm.mx>
2024-10-16 16:49:43 -0600

    Add files via upload

---
+++ EVA2_10_VALIDAR2/src/main/java/com/mycompany/eva2_10_validar2/EVA2_10_VALIDAR2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_10_validar2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aramv
+ */
+public class EVA2_10_VALIDAR2 {
+
+    public static void main(String[] args) {
+       int dia;
+        Scanner captu = new Scanner(System.in);
+        while(true){
+        System.out.println("Numero correspondiente al dia (1-7):");
+        dia = captu.nextInt();
+    
+        
+        
+        
+        }
+    }
+}
