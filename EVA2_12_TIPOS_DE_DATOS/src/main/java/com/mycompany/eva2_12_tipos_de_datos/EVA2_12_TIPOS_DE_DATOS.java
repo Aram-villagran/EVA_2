2079139d2bf1fc624eee2398e0d3f40f57ba93a2
Aram-villagran <L24550334@chihuahua2.tecnm.mx>
2024-10-21 16:49:23 -0600

    Add files via upload

---
+++ EVA2_12_TIPOS_DE_DATOS/src/main/java/com/mycompany/eva2_12_tipos_de_datos/EVA2_12_TIPOS_DE_DATOS.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_12_tipos_de_datos;
+
+/**
+ *
+ * @author aramv
+ */
+public class EVA2_12_TIPOS_DE_DATOS {
+
+    public static void main(String[] args) {
+       //byte valor = -128;
+       //byte valor2 = 127;
+       
+        //System.out.println("Valor = " + (valor));
+        //System.out.println("Valor2 = " + (valor2 + 1));
+        
+        //short val = -32768;
+       // short val2 = 32767;
+        //int x = -2147483618;
+        //int y = 2147483617;
+          
+       //int[] --> indica que es un arreglo de enteros
+       // new int[10] --> crea el arreglo con 10 espacios para enteros
+        
+        int [] arreglo = new int[10]; 
+        arreglo[0] = 100; //ponemos 100 en el entero en la posicion cero                            
+        arreglo [9] = 900; //ultima posicion (10-1)
+        
+        System.out.println(arreglo [1]);
+        System.out.println(arreglo [2]);
+        System.out.println(arreglo [3]);
+        System.out.println(arreglo [4]);
+        System.out.println(arreglo [5]);
+        System.out.println(arreglo [6]);
+        System.out.println(arreglo [7]);
+        System.out.println(arreglo [8]);
+        System.out.println(arreglo [9]);
+        System.out.println(arreglo [0]);
+        
+        
+        
+       
+    }
+}
