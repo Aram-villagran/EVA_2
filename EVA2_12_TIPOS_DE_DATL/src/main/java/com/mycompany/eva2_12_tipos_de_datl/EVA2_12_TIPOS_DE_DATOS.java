219a4a58eb650cb3b5bd245aacbc5a884a7625ac
Aram-villagran <L24550334@chihuahua2.tecnm.mx>
2024-10-21 16:49:02 -0600

    Add files via upload

---
+++ EVA2_12_TIPOS_DE_DATL/src/main/java/com/mycompany/eva2_12_tipos_de_datl/EVA2_12_TIPOS_DE_DATOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_12_tipos_de_datl;
+
+/**
+ *
+ * @author aramv
+ */
+public class EVA2_12_TIPOS_DE_DATL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
