3e68ff1abd4e6dc8ed58e1a91bdcf91c68fdf79f
Aram-villagran <L24550334@chihuahua2.tecnm.mx>
2024-10-21 16:50:06 -0600

    Add files via upload

---
+++ EVA2_16_ARREGLOS/src/main/java/com/mycompany/eva2_16_arreglos/EVA2_16_ARREGLOS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_16_arreglos;
+
+/**
+ *
+ * @author aramv
+ */
+public class EVA2_16_ARREGLOS {
+
+    public static void main(String[] args) {
+String[] diasSemana = {"Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"};
+
+for (int i = 0; i < diasSemana.length; i++){
+    System.out.print(diasSemana[i] + " - ");
+    }
+    }
+}
